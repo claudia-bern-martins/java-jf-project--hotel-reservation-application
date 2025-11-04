dbf0325d0acb34a14a85f3b53e99373de8b30409
Cláudia Martins <dev.acc.4.claudia.martins@gmail.com>
2025-11-04 22:30:30 +0000

    DateFormatter

--- hotel reservation/src/utils/DateFormatter.java
+++ hotel reservation/src/utils/DateFormatter.java
@@ -1,4 +1,34 @@
 package utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class DateFormatter {
+
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+    private static final Calendar calendar = Calendar.getInstance();
+    private static final TimeZone timezone = TimeZone.getTimeZone("Europe/Lisbon");
+    private DateFormatter(){}
+
+    public static String formatDate(Date date) {
+        formatter.setTimeZone(timezone);
+        return formatter.format(date);
+    }
+
+    public static Date getDate(String dateString) throws IllegalArgumentException {
+        calendar.setTimeZone(timezone);
+        try {
+            Date date = formatter.parse(dateString);
+            calendar.setTime(date);
+            return calendar.getTime();
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Invalid date format. Please input dates in the yyyy/MM/dd format. "
+                    + e.getLocalizedMessage());
+        }
+    }
+
+
 }
